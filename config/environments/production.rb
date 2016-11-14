71b50a2554b5bc10a0c8a1ca971f0eb23d7d55aa
Alexander Sviridov <alexander.sviridov@gmail.com>
2016-11-14 22:04:56 +0300

    deploy 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.prefix = '/weather'
+  config.assets.prefix = '/weather/assets'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
